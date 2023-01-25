c934f747ed629ae96d04a7c68f7c0dc29961e6a5
alinaincodeland <alinacuznetov@gmail.com>
2023-01-25 22:35:33 +0100

    Fixed the styles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header className="header">
       <ImCool className="header--image" />
-      <h2 className="header--title">Meme DIY</h2>
-      <h2 className="header--project">React Project</h2>
+      <h2 className="header--title">MEME DIY</h2>
+      <h2 className="header--project"> React project</h2>
     </header>
   );
 }
